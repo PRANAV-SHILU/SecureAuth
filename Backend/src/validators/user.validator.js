430f042167f86bbe53a0546c852f3311f5931c0f
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-08-13 20:17:57 +0530

    created backend  api service for change password

--- Backend/src/validators/user.validator.js
+++ Backend/src/validators/user.validator.js
@@ -35,3 +35,32 @@ export const updateProfileValidation = [
     .isLength({ max: 300 })
     .withMessage("bio cannot exceed 300 characters"),
 ];
+
+export const changePasswordValidation = [
+  check("oldPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("Current password is required"),
+
+  check("newPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("New password is required")
+    .custom((value) => {
+      if (value.includes(" ")) {
+        throw new Error("Password cannot contain spaces");
+      }
+      return true;
+    })
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .isStrongPassword({
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage(
+      "Password must include at least one uppercase letter, one lowercase letter, one digit, and one special character"
+    ),
+];
